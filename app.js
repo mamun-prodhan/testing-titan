3d4865a61a82a2309d213a324cba5080b4c04785
mamun-prodhan <mamunpr9@gmail.com>
2024-09-28 07:55:09 +0600

    test commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// function add(string){
-//     let numbers = string.split('+');
-//     let result = parseInt(numbers[0]) + parseInt(numbers[1]);
-//     return result;
-// }
-// console.log(add('1+2'));
+function add(string){
+    let numbers = string.split('+');
+    let result = parseInt(numbers[0]) + parseInt(numbers[1]);
+    return result;
+}
+console.log(add('1+2'));
 
 function flat(loshu){
     let result = loshu[0][0].concat(loshu[0][1]);
